e0ad1d4cefe2fae42365fd041a65e633ce3eb560
liucong55 <liucong55@163.com>
2016-04-18 13:50:01 +0800

    配置文件写入

--- Install/Write/index.php
+++ Install/Write/index.php
@@ -1,10 +1,6 @@
 <?php
 /**
  * 项目入口
- * @author LuoBoss
- * @copyright ©2013-2014 PESCMS
- * @license http://www.pescms.com/license
- * @version 1.0
  */
 define('ITEM', 'App');
 define('THEME', dirname(__FILE__).'/Theme');
